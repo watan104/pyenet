1ab9d40865f914216d8c670ea21962224c7d42b6
watan104 <96546018+watan104@users.noreply.github.com>
2025-09-15 02:39:15 +0300

    refactor: simplify setup.py for enet extension

--- setup.py
+++ setup.py
@@ -2,46 +2,47 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-import glob
-import sys
+import glob, sys
 
-source_files = ["enet.pyx"]
+source_files = ["enet.pyx"] + glob.glob("enet/*.c")
 
-_enet_files = glob.glob("enet/*.c")
+if len(source_files) == 1:
+    sys.exit(
+        "ENet source files not found.\n"
+        "Download: https://github.com/GTPSHAX/how-to-fix-growtopia-5.15\n"
+        "Credit: GTPSHAX, ikazu and others."
+    )
 
-if not _enet_files:
-    print("latest enet source developed for growtopia")
-    print("download it from: https://github.com/GTPSHAX/how-to-fix-growtopia-5.15")
-    print("credit: GTPSHAX, ikazu, and other contributors")
-    sys.exit(1)
-
-source_files.extend(_enet_files)
-
-define_macros = [('HAS_POLL', None),
-                 ('HAS_FCNTL', None),
-                 ('HAS_MSGHDR_FLAGS', None),
-                 ('HAS_SOCKLEN_T', None) ]
-
+define_macros = [
+    ('HAS_POLL', None),
+    ('HAS_FCNTL', None),
+    ('HAS_MSGHDR_FLAGS', None),
+    ('HAS_SOCKLEN_T', None)
+]
 libraries = []
 
-if sys.platform == 'win32':
-    define_macros.extend([('WIN32', None)])
-    libraries.extend(['ws2_32', 'Winmm'])
-
-if sys.platform != 'darwin':
-    define_macros.extend([('HAS_GETHOSTBYNAME_R', None), ('HAS_GETHOSTBYADDR_R', None)])
+if sys.platform == "win32":
+    define_macros.append(('WIN32', None))
+    libraries += ['ws2_32', 'Winmm']
+elif sys.platform != "darwin":
+    define_macros += [
+        ('HAS_GETHOSTBYNAME_R', None),
+        ('HAS_GETHOSTBYADDR_R', None)
+    ]
 
 ext_modules = [
     Extension(
         "enet",
-        extra_compile_args=["-O3"],
         sources=source_files,
         include_dirs=["enet/include/"],
         define_macros=define_macros,
-        libraries=libraries)]
+        libraries=libraries,
+        extra_compile_args=["-O3"],
+    )
+]
 
 setup(
-  name = 'enet',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+    name="enet",
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
 )
