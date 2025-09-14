d2cc19155327b60db7f04575d21fb131f27102a5
watan104 <96546018+watan104@users.noreply.github.com>
2025-09-15 02:29:29 +0300

    Clean code rewrite for ENet Client Test.

--- test_client.py
+++ test_client.py
@@ -2,6 +2,7 @@ import enet
 import random
 import sys
 
+# Seed
 try:
     random.seed(sys.argv[1])
 except IndexError:
@@ -10,81 +11,80 @@ except IndexError:
 SHUTDOWN_MSG = b"SHUTDOWN"
 MSG_NUMBER = 10
 
+# Create host (client)
 host = enet.Host(None, 1, 0, 0, 0)
 host.checksum = enet.ENET_CRC32
-peer = host.connect(enet.Address(b"localhost", 54301), 1)
+
+peer = host.connect(enet.Address(b"localhost", 17091), 1)
 
 counter = 0
 run = True
-while run:
-    event = host.service(1000)
-    if event.type == enet.EVENT_TYPE_CONNECT:
-        print("%s: CONNECT" % event.peer.address)
-    elif event.type == enet.EVENT_TYPE_DISCONNECT:
-        print("%s: DISCONNECT" % event.peer.address)
-        run = False
-        continue
-    elif event.type == enet.EVENT_TYPE_RECEIVE:
-        print("%s: IN:  %r" % (event.peer.address, event.packet.data))
-        continue
-    msg = bytes(bytearray([random.randint(0, 255) for i in range(40)]))
-    packet = enet.Packet(msg)
-    peer.send(0, packet)
 
-    counter += 1
-    if counter >= MSG_NUMBER:
-        msg = SHUTDOWN_MSG
+try:
+    while run:
+        event = host.service(1000)
+        if event.type == enet.EVENT_TYPE_CONNECT:
+            print(f"{event.peer.address}: CONNECT")
+        elif event.type == enet.EVENT_TYPE_DISCONNECT:
+            print(f"{event.peer.address}: DISCONNECT")
+            run = False
+            continue
+        elif event.type == enet.EVENT_TYPE_RECEIVE:
+            print(f"{event.peer.address}: IN: {event.packet.data!r}")
+            continue
+
+        msg = bytes([random.randint(0, 255) for _ in range(40)])
         peer.send(0, enet.Packet(msg))
-        host.service(100)
-        peer.disconnect()
+        print(f"{peer.address}: OUT: {msg!r}")
 
-    print("%s: OUT: %r" % (peer.address, msg))
+        counter += 1
+        if counter >= MSG_NUMBER:
+            peer.send(0, enet.Packet(SHUTDOWN_MSG))
+            host.service(100)
+            peer.disconnect()
+except KeyboardInterrupt:
+    print("\nClient manually stopped.")
 
-
-# Part of the test to do with intercept callback and socket.send
-peer = host.connect(enet.Address(b"localhost", 54301), 1)
+# --- Intercept ---
+peer = host.connect(enet.Address(b"localhost", 17091), 1)
 shutdown_scheduled = False
 run = True
 
-
 def receive_callback(address, data):
     global shutdown_scheduled
-
     if shutdown_scheduled:
         return
-
     if data == b"\xff\xff\xff\xffstatusResponse\n":
-        # if the test gets to this point, it means it has passed. Disconnect is a clean up
         shutdown_scheduled = True
     else:
-        # error messages are not propagating
-        # through cython
-        print("data != statusResponse. Instead of expected, got %r" % data)
+        print(f"Unexpected data: {data!r}")
         assert False
 
+host.intercept = receive_callback
 
-while run:
-    event = host.service(1000)
-    if event.type == enet.EVENT_TYPE_CONNECT:
-        print("%s: CONNECT" % event.peer.address)
-        msg = bytes(bytearray([random.randint(0, 255) for i in range(40)]))
-        packet = enet.Packet(msg)
-        peer.send(0, packet)
+try:
+    while run:
+        event = host.service(1000)
+        if event.type == enet.EVENT_TYPE_CONNECT:
+            print(f"{event.peer.address}: CONNECT")
+            msg = bytes([random.randint(0, 255) for _ in range(40)])
+            peer.send(0, enet.Packet(msg))
 
-        host.intercept = receive_callback
-    elif event.type == enet.EVENT_TYPE_DISCONNECT:
-        print("%s: DISCONNECT" % event.peer.address)
-        run = False
-        continue
-    elif event.type == enet.EVENT_TYPE_RECEIVE:
-        print("%s: IN:  %r" % (event.peer.address, event.packet.data))
-        continue
+        elif event.type == enet.EVENT_TYPE_DISCONNECT:
+            print(f"{event.peer.address}: DISCONNECT")
+            run = False
+            continue
 
-    if shutdown_scheduled:
-        msg = SHUTDOWN_MSG
-        peer.send(0, enet.Packet(msg))
-        host.service(100)
-        peer.disconnect()
-        continue
+        elif event.type == enet.EVENT_TYPE_RECEIVE:
+            print(f"{event.peer.address}: IN: {event.packet.data!r}")
+            continue
 
-    host.socket.send(peer.address, b"\xff\xff\xff\xffgetstatus\x00")
+        if shutdown_scheduled:
+            peer.send(0, enet.Packet(SHUTDOWN_MSG))
+            host.service(100)
+            peer.disconnect()
+            continue
+
+        host.socket.send(peer.address, b"\xff\xff\xff\xffgetstatus\x00")
+except KeyboardInterrupt:
+    print("\nClient manually stopped.")
